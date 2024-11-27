7b4d45c1125d0bef92176c9f215ea3d8bc018580
artemetra <artemlukin15@gmail.com>
2024-11-27 14:59:24 +0100

    implement add_to_zeroes

--- main.py
+++ main.py
@@ -15,24 +15,36 @@ class fractal2D:
         self.f = f
         self.f_prime = f_prime
 
-    def newtons_method(self, guess: Vector) -> Vector:
+    def newtons_method(self, guess: Vector) -> Optional[Vector]:
         """Task 2: Performs Newton's method on function `self.f` using `guess` as a starting point."""
-        # TODO
-        
+        # TODO: handle divergence
         x_n = guess
         while np.linalg.norm(self.f(x_n)) > tol:
             x_n = x_n - np.linalg.inv(self.get_jacobian_matrix()) @ self.f(x_n)
+            if np.linalg.norm(x_n) > 100000: # TODO: make it a reasonable number
+                return None
 
         return x_n
 
-    def add_to_zeroes(self, guess: Vector):
+    def add_to_zeroes(self, guess: Vector) -> Optional[int]:
         """Task 3"""
         # TODO
-        pass
+        new_zero = self.newtons_method(guess)
+        if new_zero is None:
+            # newton's method did not converge
+            return None
+        
+        for idx, z in enumerate(self.zeroes):
+            if np.abs(new_zero-z) < tol:
+                return idx # index of the zero
+        
+        self.zeroes.append(new_zero)
+        return len(self.zeroes)-1 # index of the last zero 
 
 
     def get_jacobian_matrix(self, x_n: Vector) -> np.ndarray:
         """For finding the derivative"""
+        jac = ...
         self.f([])
         pass
     
