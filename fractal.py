6e724c9f93783370199b79a102b6e1d272ec4168
yannick <yannick2.kapelle@gmail.com>
2024-12-13 16:18:23 +0100

    Code reorganisation: Split into two classes

--- fractal.py
+++ fractal.py
@@ -25,7 +25,6 @@ def evaluate_jacobian(jacobian: JacobianType, val: Vector) -> np.ndarray:
         ]
     )
 
-
 class fractal2D:
     zeroes: list[Vector] = []
 
@@ -167,55 +166,3 @@ class fractal2D:
                 return None, -1
 
         return x_n, i
-
-
-def F(x):
-    x1 = x[0]
-    x2 = x[1]
-    # it is (empirically) faster to return a list instead of numpy array
-    return [x1**3 - 3 * x1 * x2**2 - 1, 3 * x1**2 * x2 - x2**3]
-
-
-def F1_Task8(x):
-    x1 = x[0]
-    x2 = x[1]
-    return [x1**3 - 3 * x1 * x2**2 - 2 * x1 - 2, 3 * x1**2 * x2 - x2**3 - 2 * x2]
-
-
-def F2_Task8(x):
-    x1 = x[0]
-    x2 = x[1]
-    return [
-        x1**8
-        - 28 * x1**6 * x2**2
-        + 70 * x1**4 * x2**4
-        + 15 * x1**4
-        - 28 * x1**2 * x2**6
-        - 90 * x1**2 * x2**2
-        + x2**8
-        + 15 * x2**4
-        - 16,
-        8 * x1**7 * x2
-        - 56 * x1**5 * x2**3
-        + 56 * x1**3 * x2**5
-        + 60 * x1**3 * x2
-        - 8 * x1 * x2**7
-        - 60 * x1 * x2**3,
-    ]
-
-
-def main():
-    # the pre-calculated jacobian for function `F`
-    jac_F = [  # noqa: F841
-        [lambda x, y: 3 * x**2 - 3 * y**2, lambda x, y: -6 * x * y],
-        [lambda x, y: 6 * x * y, lambda x, y: 3 * x**2 - 3 * y**2],
-    ]
-    frac = fractal2D(F1_Task8)
-    start = datetime.now()
-    print(f"start: {start}")
-    frac.plot(N=100, coord=(-1, 1, -1, 1), simplified=False, show=False, iter=True)
-    print(f"duration: {datetime.now()-start}")
-
-
-if __name__ == "__main__":
-    main()
